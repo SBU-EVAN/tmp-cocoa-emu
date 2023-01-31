910aa6bda5533d2a41948fbdd047e7450b03b974
Evan <43404486+evansaraivanov@users.noreply.github.com>
2023-01-31 12:21:58 -0500

    remove cocoa_dependence

--- cocoa_emu/__init__.py
+++ cocoa_emu/__init__.py
@@ -1,5 +1,5 @@
 from .config import Config
 from .utils import *
-from .cocoa_model import CocoaModel
+#from .cocoa_model import CocoaModel
 #from .gp_emulator import GPEmulator
 from .nn_emulator import NNEmulator, nn_pca_emulator
